2205b18988acfb705edb42d570bebc2bec78cc45
jouinfogix <jingsong.ou@precisely.com>
2024-10-03 09:34:48 -0500

    Delete Github_Repo/devenv/cloud/tools directory

--- Github_Repo/devenv/cloud/tools/python/utils/get_patches_list.sh
+++
@@ -1,5 +0,0 @@
-ls -al /opt/cloudramp/*.json | grep -v Webserver | cut -d' ' -f10 | cut -d'/' -f4 | cut -d'.' -f1 > envs.list 
-
-python list_patch.py  | tee patches.list
-mail  -s "Infogix Cloud Environment Patch status" rli@infogix.com bprocek@infogix.com < patches.list 
-# rm ./envs.list
